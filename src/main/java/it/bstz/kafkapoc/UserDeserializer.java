223c10b103a08fadf73f7c7095d2b71db23782db
Tiziano Basile <tiz.basile@gmail.com>
2023-06-29 09:37:34 +0200

    more logs

--- src/main/java/it/bstz/kafkapoc/UserDeserializer.java
+++ src/main/java/it/bstz/kafkapoc/UserDeserializer.java
@@ -1,11 +1,21 @@
 package it.bstz.kafkapoc;
 
 import io.quarkus.kafka.client.serialization.ObjectMapperDeserializer;
+import io.quarkus.logging.Log;
 
-public class UserDeserializer extends ObjectMapperDeserializer<User>
-{
-   public UserDeserializer()
-   {
+public class UserDeserializer extends ObjectMapperDeserializer<User> {
+   public UserDeserializer() {
       super(User.class);
+   }
+
+   @Override
+   public User deserialize(String topic, byte[] data) {
+      Log.info("=====UserDeserialize DESERIALIZING=====");
+      Log.info("=====TOPIC: " + topic + "=====");
+      Log.info("=====DATA: " + data + "=====");
+      User u = super.deserialize(topic, data);
+      Log.info("=====DESERIALIZED DATA: " + u + "=====");
+      return u;
+
    }
 }
--- src/main/java/it/bstz/kafkapoc/UserSerializer.java
+++ src/main/java/it/bstz/kafkapoc/UserSerializer.java
@@ -3,16 +3,26 @@ package it.bstz.kafkapoc;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.quarkus.kafka.client.serialization.ObjectMapperSerializer;
+import io.quarkus.logging.Log;
 
 import static com.fasterxml.jackson.databind.SerializationFeature.FAIL_ON_EMPTY_BEANS;
 import static com.fasterxml.jackson.databind.SerializationFeature.WRITE_DATES_AS_TIMESTAMPS;
 
-public class UserSerializer extends ObjectMapperSerializer<User>
-{
-   public UserSerializer()
-   {
+public class UserSerializer extends ObjectMapperSerializer<User> {
+   public UserSerializer() {
       super(new ObjectMapper().findAndRegisterModules()
-               .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-               .disable(WRITE_DATES_AS_TIMESTAMPS, FAIL_ON_EMPTY_BEANS));
+              .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+              .disable(WRITE_DATES_AS_TIMESTAMPS, FAIL_ON_EMPTY_BEANS));
+   }
+
+   @Override
+   public byte[] serialize(String topic, User data) {
+      Log.info("=====UserSerializer SERIALIZING=====");
+      Log.info("=====TOPIC: " + topic + "=====");
+      Log.info("=====USER: " + data + "=====");
+      byte[] serialized = super.serialize(topic, data);
+      Log.info("=====UserSerializer OBJECT SERIALIZED=====");
+      Log.info(serialized);
+      return serialized;
    }
 }
